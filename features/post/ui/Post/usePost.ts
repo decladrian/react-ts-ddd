79cb688baae28ca7edf2f1fed56310bbc013739b
decladrian <adrian@declarando.es>
2022-02-14 18:56:39 +0100

    1 file modified

--- features/post/ui/Post/usePost.ts
+++ features/post/ui/Post/usePost.ts
@@ -5,7 +5,8 @@ export const usePost = () => {
   const [post, setPost] = useState(null);
 
   useEffect(() => {
-    PostQry(1).then((post) => setPost(post));
+    const mockPostId = 1;
+    PostQry(mockPostId).then((post) => setPost(post));
   }, []);
 
   return {
