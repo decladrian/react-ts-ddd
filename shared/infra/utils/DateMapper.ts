9da4f16e78882435a54bf1fdef3580ea2afcac60
decladrian <adrian@declarando.es>
2022-02-15 17:44:12 +0100

    1 file modified

--- shared/infra/utils/DateMapper.ts
+++ shared/infra/utils/DateMapper.ts
@@ -1,10 +1,12 @@
 type DateFormat = 'DD-MM-YYYY' | 'YYYY-MM-DD' | 'YYYY-MM' | 'MM-YYYY';
 
+const mockStringDate = '2022-20-20';
+
 export class DateMapper {
   static nativeDateToStringFormat(
     date: Date,
     format: DateFormat = 'DD-MM-YYYY'
   ): string {
-    return '2022-20-20';
+    return mockStringDate;
   }
 }
