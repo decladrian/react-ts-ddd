cc00cb08a166b66467a46d7e0612e223a3ed3bac
decladrian <adrian@declarando.es>
2022-02-15 20:22:48 +0100

    1 file modified

--- features/post/application/PostController.ts
+++ features/post/application/PostController.ts
@@ -4,26 +4,29 @@ import { ValidatePost } from './ValidatePost';
 
 export class PostController implements PostModels.useCases {
   private readonly repository = container.postRepository;
+  // Duda sobre analytics: ¿application o infra?
   private readonly analytics = container.analytics;
 
   save(post) {
     if (!ValidatePost(post)) {
       throw new Error('Invalid data');
     }
-    this.analytics.send('POST', {});
+    this.analytics.send('SAVE_POST', post);
     return this.repository.save(post);
   }
 
   like(id: number) {
-    this.analytics.send('POST', {});
+    this.analytics.send('LIKE_POST', { id });
     return this.repository.like(id);
   }
 
   fetch(id: number) {
+    this.analytics.send('POST', { id });
     return this.repository.fetch(id);
   }
 
   findAll() {
+    this.analytics.send('POST', {});
     return this.repository.findAll();
   }
 }
