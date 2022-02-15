7bd1ac92bb918864db1996c85d3434ae8ee9bcf3
decladrian <adrian@declarando.es>
2022-02-15 17:31:57 +0100

    1 file modified

--- features/signin/infra/LoginRepository.ts
+++ features/signin/infra/LoginRepository.ts
@@ -1,7 +1,7 @@
 import { AuthModel } from '../domain/AuthModel';
 import { LoginMock } from '../domain/mocks/login';
 
-export class LoginRepository implements AuthModel.repository {
+export class LoginRepository implements AuthModel.useCases {
   login = async (form) => {
     return LoginMock;
   };
