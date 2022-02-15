9ae80e4184c2ef729bf70e2593decffdd08accfc
decladrian <adrian@declarando.es>
2022-02-15 19:38:47 +0100

    1 file modified

--- container.ts
+++ container.ts
@@ -1,14 +1,18 @@
 import { PostRepository } from './features/post/infra/PostRepository';
 import { PostRepositoryMock } from './features/post/infra/PostRepositoryMock';
 import { LoginRepository } from './features/signin/infra/LoginRepository';
+import { Analytics } from './shared/infra/utils/Analytics';
 
 const registry = {
+  // Repositories
   postRepository: new PostRepository(),
   loginRepository: new LoginRepository(),
+  // libs
+  analytics: Analytics,
 };
 
 const mocks = {
-  postRepository: new PostRepositoryMock(),
+  //postRepository: new PostRepositoryMock(),
 };
 
 export const container = { ...registry, ...mocks };
