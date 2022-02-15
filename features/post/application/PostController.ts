cf4b8085cfe776a70a656e3a03eece9ed4504928
decladrian <adrian@declarando.es>
2022-02-15 17:39:17 +0100

    1 file modified, 1 added

--- features/post/application/PostController.ts
+++ features/post/application/PostController.ts
@@ -1,12 +1,12 @@
 import { container } from '../../../container';
 import { PostModels } from '../domain/PostModels';
+import { ValidatePost } from './ValidatePost';
 
 export class PostController implements PostModels.useCases {
   private readonly repository = container.postRepository;
 
   save(post) {
-    if (false) {
-      // !validate(post)
+    if (!ValidatePost(post)) {
       throw new Error('Invalid data');
     }
     return this.repository.save(post);
---
+++ features/post/application/ValidatePost.ts
@@ -0,0 +1,5 @@
+import { PostModels } from '../domain/PostModels';
+
+export const ValidatePost = (post: PostModels.model): boolean => {
+  return true;
+};
