f5cf17bc3bce6673238ba84525a5f0fa68ec08bd
decladrian <adrian@declarando.es>
2022-02-17 22:43:26 +0100

    2 files modified

--- shared/application/Controller.ts
+++ shared/application/Controller.ts
@@ -3,4 +3,5 @@ import { container } from '../../container';
 export class Controller {
   protected readonly query = container.Query;
   protected readonly command = container.Command;
+  protected readonly logger = container.Logger;
 }
--- shared/infra/Command.ts
+++ shared/infra/Command.ts
@@ -1,11 +1,14 @@
+import { container } from '../../container';
 import { UseCase } from './UseCase';
 
+const Logger = container.Logger;
 export class Command {
   static async execute<T>(
     key: string,
     useCaseCall: () => Promise<T>,
     settings?: { params?: any }
   ): Promise<T> {
+    Logger.log('RUN COMMAND:', key);
     try {
       var data = await useCaseCall();
     } catch (e) {
