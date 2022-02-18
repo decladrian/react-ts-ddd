71d27e9ba0b6184a760a17bf6615ab20c6460bcd
decladrian <adrian@declarando.es>
2022-02-18 21:33:52 +0100

    1 file modified

--- features/post/application/PostController.ts
+++ features/post/application/PostController.ts
@@ -6,31 +6,37 @@ import { ValidatePost } from './ValidatePost';
 export class PostController extends Controller implements PostModels.useCases {
   private readonly repository = container.postRepository;
 
+  private prefix = 'POST';
+
   save(post) {
     if (!ValidatePost(post)) {
       throw new Error('Invalid Post');
     }
 
-    return this.command.execute('SAVE_POST', () => this.repository.save(post), {
-      params: { post },
-    });
+    return this.command.execute(
+      this.prefix.concat('_SAVE'),
+      () => this.repository.save(post),
+      {
+        params: { post },
+      }
+    );
   }
 
   like(id: number) {
-    return this.command.execute('LIKE_POST', () => this.repository.like(id), {
+    return this.command.execute(this.prefix.concat("_LIKE"), () => this.repository.like(id), {
       params: { id },
     });
   }
 
   find(id: number) {
-    return this.query.execute('POST', () => this.repository.fetch(id), {
+    return this.query.execute(this.prefix, () => this.repository.find(id), {
       params: { id },
     });
   }
 
   findAll() {
     return this.query.execute(
-      'POST_COLLECTION',
+      this.prefix.concat("_COLLECTION"),
       () => this.repository.findAll(),
       {}
     );
