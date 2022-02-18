3e31b00fdc024761a1a8f3b0591809b0a9215508
decladrian <adrian@declarando.es>
2022-02-18 21:53:21 +0100

    1 file modified

--- features/profile/infra/ProfileMapper.ts
+++ features/profile/infra/ProfileMapper.ts
@@ -3,13 +3,13 @@ import { ProfileFormModel } from './ProfileFormModel';
 import { DateMapper } from '../../../shared/infra/utils/DateMapper';
 
 export class ProfileMapper {
-  static formToDTO(form: ProfileFormModel): UserModel.editResponse {
+  static formToDTO(form: ProfileFormModel): UserModel.editRequest {
     return {
       ...form,
       birthday: DateMapper.nativeDateToStringFormat(
         form.birthday,
         'YYYY-MM-DD'
-      ),
+      ),,
     };
   }
 }
