2675882dd522c86193b3faed49c56e91e10afb9e
decladrian <adrian@declarando.es>
2022-02-19 15:54:58 +0100

    1 file modified

--- features/post/domain/PostEntity.ts
+++ features/post/domain/PostEntity.ts
@@ -21,5 +21,16 @@ export class PostEntity extends Entity<PostModels.model> {
     super(data);
   }
 
-  
+  validate() {
+    const { title, content } = this.data;
+    this.errors = {};
+    if (title.length < 3) {
+      this.errors.tile = 'Min lenght 3';
+    }
+    if (content.length < 3) {
+      this.errors.content = 'Min lenght 3';
+    }
+
+    return Object.keys(this.errors).length === 0;
+  }
 }
