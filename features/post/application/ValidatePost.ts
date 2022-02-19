01061eef7158024acaba00211b1842df69496b3a
decladrian <adrian@declarando.es>
2022-02-19 21:24:27 +0100

    2 files deleted

--- features/post/application/ValidatePost.ts
+++
@@ -1,7 +0,0 @@
-import { PostModels } from '../domain/PostModels';
-
-export const ValidatePost = (post: PostModels.model): boolean => {
-  // Validar formulario
-  // Tabien! confio en el usuario
-  return true;
-};
--- features/profile/application/Validation.ts
+++
@@ -1,6 +0,0 @@
-import { UserModel } from '../domain/UserModel';
-
-export const ValidateUser = (profile: UserModel.editRequest) => {
-  // Confio en el usuario :-)
-  return true;
-};
