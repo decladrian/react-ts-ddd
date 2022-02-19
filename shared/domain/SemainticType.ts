efdd7b19766b76955178c2350f2448da5a4400ab
decladrian <adrian@declarando.es>
2022-02-19 16:24:10 +0100

    1 file modified

--- shared/domain/SemainticType.ts
+++ shared/domain/SemainticType.ts
@@ -2,7 +2,7 @@ export namespace SemanticTypes {
   export type EMAIL = string;
   export type DATEFORMAT_YYYY_MM_DDD = string;
   export type DATEFORMAT_DD_MM_YYYY = string;
+  export type DATETIME = number;
   export type ID = number;
   export type NIF = string;
-  export type DATETIME = number;
 }
