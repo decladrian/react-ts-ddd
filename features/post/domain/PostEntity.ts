9d115c39e6d60962fb299a5a41b24a5dc88e8928
decladrian <adrian@declarando.es>
2022-02-20 20:33:54 +0100

    1 file modified

--- features/post/domain/PostEntity.ts
+++ features/post/domain/PostEntity.ts
@@ -6,10 +6,10 @@ export class PostEntity extends Entity<PostModels.model> {
     const { title, content } = this.data;
     this.errors = {};
     if (title.length < 3) {
-      this.errors.title = 'Min lenght 3';
+      this.errors.title = 'Min length 3';
     }
     if (content.length < 3) {
-      this.errors.content = 'Min lenght 3';
+      this.errors.content = 'Min length 3';
     }
 
     return Object.keys(this.errors).length === 0;
