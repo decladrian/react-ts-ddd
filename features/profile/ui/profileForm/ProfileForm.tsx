a0dd2d8321d3af4788cea1db6dac2b7e5ffa866a
decladrian <adrian@declarando.es>
2022-02-20 22:01:35 +0100

    2 files modified

--- features/profile/ui/profileForm/ProfileForm.tsx
+++ features/profile/ui/profileForm/ProfileForm.tsx
@@ -1,1 +1,29 @@
-// Profile Form
+import React, { ReactNode } from 'react';
+import { useProfileForm } from './useProfileForm';
+
+export const ProfileForm: ReactNode = ({ navigate }) => {
+  const { form, changeValue, submit, errors } = useProfileForm(navigate);
+
+  return (
+    <main>
+      <div>
+        <b>Username </b>
+        <input
+          value={form.username}
+          onChange={(e) => changeValue('username', e.currentTarget.value)}
+        />
+        {errors?.username}
+      </div>
+      <div>
+        <b>Email </b>
+        <input
+          value={form.content}
+          onChange={(e) => changeValue('email', e.currentTarget.value)}
+        />
+        {errors?.email}
+      </div>
+
+      <button onClick={submit}>Editar</button>
+    </main>
+  );
+};
--- features/profile/ui/profileForm/useProfileForm.ts
+++ features/profile/ui/profileForm/useProfileForm.ts
@@ -1,1 +1,33 @@
-// Profile Form hook
+import { useEffect, useState } from 'react';
+import { container } from '../../../../container';
+import { ErrorTypes } from '../../../../shared/domain/error/ErrorTypes';
+import { ProfileController } from '../../application/ProfileController';
+import { useProfileFormReducer } from './useProfileFormReducer';
+
+export const useProfileForm = (navigate) => {
+  const [errors, setErrors] = useState<any>({});
+  useEffect(() => {
+    const subscription = container.postSubscriber.$subject.subscribe(
+      (data: object) => {}
+    );
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  const { form, changeValue } = useProfileFormReducer();
+
+  const submit = async () => {
+    setErrors({});
+    try {
+      const result = await new ProfileController().edit(form);
+      navigate('post');
+    } catch (e: any) {
+      if (e.type === ErrorTypes.invalidDataExecption) {
+        setErrors(e.errors);
+      }
+    }
+  };
+
+  return { form, changeValue, submit, errors };
+};
