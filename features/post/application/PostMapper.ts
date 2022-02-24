22826be9728f49e589958f3651b4cfd7484fb0d8
decladrian <adrian@declarando.es>
2022-02-24 21:36:44 +0100

    1 file modified

--- features/post/application/PostMapper.ts
+++ features/post/application/PostMapper.ts
@@ -7,7 +7,7 @@ export class PostMapper {
   static modelToDto(model: PostModels.model): PostModels.rawModel {
     return {
       ...model,
-      created_at: PostMapper.DateMapper.ativeDateToStringFormat(
+      created_at: PostMapper.DateMapper.nativeDateToStringFormat(
         model.created_at
       ),
     };
