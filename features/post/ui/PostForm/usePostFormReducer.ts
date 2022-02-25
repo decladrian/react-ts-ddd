c4a7450bd5a98f7f386af0b1cc296a06b34e15cc
decladrian <adrian@declarando.es>
2022-02-25 14:59:54 +0100

    1 file modified

--- features/post/ui/PostForm/usePostFormReducer.ts
+++ features/post/ui/PostForm/usePostFormReducer.ts
@@ -1,4 +1,4 @@
-import { useReducer, useState } from 'react';
+import { useReducer } from 'react';
 //import { PostEntity } from '../../domain/PostEntity';
 import { PostModels } from '../../domain/PostModels';
 
